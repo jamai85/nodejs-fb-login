6e59aefb229b0a51a65c2a924d919156f8078aad
Gabe Smith (desktop) <gabriel.smith01@gmail.com>
2014-09-18 10:27:15 -0400

    modified fb login to access server side

---
+++ public/js/fb.login.js
@@ -0,0 +1,80 @@
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.
+        retrieveUserInfo(response);
+    } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+        $('#status').html = 'Please log into this app.';
+    } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+        $('#status').html = 'Please log into Facebook.';
+    }
+}
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '328739433954471',
+        cookie     : true,  // enable cookies to allow the server to access
+        // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.1' // use version 2.1
+    });
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+/**
+ * Pass FB response object to server.  Update user area with image and name
+ * @param response
+ */
+function retrieveUserInfo(response) {
+    console.log('getting data')
+    $.post(
+        '/user',
+        {
+            'accessToken': response.authResponse.accessToken,
+            'userId': response.authResponse.userId
+        },
+        function(data) {
+            //handle success
+            if (data.error == true) {
+
+            } else if (data.error == false) {
+                // update #status with image and name
+            }
+        },
+        function() {
+            // handle connection error
+        }
+    );
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Facebook Login' });
+});
+
+router.post('/user', function(req, res) {
+    var accessToken = req.body.accessToken,
+        userId      = req.body.UserID;
+
+    res.end(JSON.stringify(req.body));
 });
 
 module.exports = router;
